945e0586030f6cb9cd77580616a364f2325b6b18
gpiancastelli <gpiancastelli@localhost>
2009-10-06 11:30:16 +0200

    Add Google Analytics tracking.

---
+++ j/ga.js
@@ -0,0 +1,1 @@
+<script>var gaJsHost="http://www.";document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script>try{var pageTracker=_gat._getTracker("UA-10482652-1");pageTracker._trackPageview();}catch(err){}</script>
