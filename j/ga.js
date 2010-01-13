ddb1a4405395dc94702fc688f400df454c14e097
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2010-01-13 18:04:49 +0100

    Catching up with six weeks worth of minor adjustments to the original DiP3.

--- j/ga.js
+++ j/ga.js
@@ -1,1 +1,1 @@
-<script>var gaJsHost="http://www.";document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script>try{var pageTracker=_gat._getTracker("UA-10482652-1");pageTracker._trackPageview();}catch(err){}</script>
+<script>_gaq=[['_setAccount','UA-10482652-1'],['_trackPageview']];(function(){var g=document.createElement('script');g.src='//www.google-analytics.com/ga.js';g.setAttribute('async','true');document.documentElement.firstChild.appendChild(g);})();</script>
